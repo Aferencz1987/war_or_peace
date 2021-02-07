b6b9284c40eb23df1046d4444702962a08ff74d5
aferencz1987 <alex.ferencz6@gmail.com>
2021-02-07 15:55:32 -0700

    Update war_or_peace_runner.rb

--- lib/war_or_peace_runner.rb
+++ lib/war_or_peace_runner.rb
@@ -3,19 +3,151 @@ require './lib/deck'
 require './lib/player'
 require './lib/turn'
 
+@shuffled_deck = @game_deck.shuffle
 
-card.new
-deck.new
-player1.new
-player2.new
 
-player1
 
-users = User.all
-users.each do |element|
-  element.age = rand(10..99).to_s
-  element.save!
+@player1_deck = []
+@player1_deck << @shuffled_deck.slice[0..25]
+@player2_deck = []
+@player2_deck << @shuffled_deck.slice[26..51]
+player1 = Player.new("Wanda", deck1)
+player2 = Player.new("Cowboy", deck2)
+
+def start
+  p "Welcome to War! (or Peace) This game will be played with 52 cards.
+   The players today are Megan and Aurora.
+   Type 'GO' to start the game!"
+
+   @input = gets.chomp
+
+   if @input == "GO"
+     player1_deck.turn
+     player2_deck.turn
+
+
+
+
+
+
+
+
+
+
+
+
+
+  else
+    p "Hey maybe you should try typing GO instead"
+  end
 end
 
-card = Card.new
-card.each do
+
+
+card1 = Card.new(:heart, 'Ace', 14)
+card2 = Card.new(:heart, '2', 2)
+card3 = Card.new(:heart, '3', 3)
+card4 = Card.new(:heart, '4', 4)
+card5 = Card.new(:heart, '5', 5)
+card6 = Card.new(:heart, '6', 6)
+card7 = Card.new(:heart, '7', 7)
+card8 = Card.new(:heart, '8', 8)
+card9 = Card.new(:heart, '9', 9)
+card10 = Card.new(:heart, '10', 10)
+card11 = Card.new(:heart, 'Jack', 11)
+card12 = Card.new(:heart, 'Queen', 12)
+card13 = Card.new(:heart, 'King', 13)
+card14 = Card.new(:diamond, 'Ace', 14)
+card15 = Card.new(:diamond, '2', 2)
+card16 = Card.new(:diamond, '3', 3)
+card17 = Card.new(:diamond, '4', 4)
+card18 = Card.new(:diamond, '5', 5)
+card19 = Card.new(:diamond, '6', 6)
+card20 = Card.new(:diamond, '7', 7)
+card21 = Card.new(:diamond, '8', 8)
+card22 = Card.new(:diamond, '9', 9)
+card23 = Card.new(:diamond, '10', 10)
+card24 = Card.new(:diamond, 'Jack', 11)
+card25 = Card.new(:diamond, 'Queen', 12)
+card26 = Card.new(:diamond, 'King', 13)
+card27 = Card.new(:spade, 'Ace', 14)
+card28 = Card.new(:spade, '2', 2)
+card29 = Card.new(:spade, '3', 3)
+card30 = Card.new(:spade, '4', 4)
+card31 = Card.new(:spade, '5', 5)
+card32 = Card.new(:spade, '6', 6)
+card33 = Card.new(:spade, '7', 7)
+card34 = Card.new(:spade, '8', 8)
+card35 = Card.new(:spade, '9', 9)
+card36 = Card.new(:spade, '10', 10)
+card37 = Card.new(:spade, 'Jack', 11)
+card38 = Card.new(:spade, 'Queen', 12)
+card39 = Card.new(:spade, 'King', 13)
+card40 = Card.new(:club, 'Ace', 14)
+card41 = Card.new(:club, '2', 2)
+card42 = Card.new(:club, '3', 3)
+card43 = Card.new(:club, '4', 4)
+card44 = Card.new(:club, '5', 5)
+card45 = Card.new(:club, '6', 6)
+card46 = Card.new(:club, '7', 7)
+card47 = Card.new(:club, '8', 8)
+card48 = Card.new(:club, '9', 9)
+card49 = Card.new(:club, '10', 10)
+card50 = Card.new(:club, 'Jack', 11)
+card51 = Card.new(:club, 'Queen', 12)
+card52 = Card.new(:club, 'King', 13)
+
+@game_deck = [
+  card1,
+  card2,
+  card3,
+  card4,
+  card5,
+  card6,
+  card7,
+  card8,
+  card9,
+  card10,
+  card11,
+  card12,
+  card13,
+  card14,
+  card15,
+  card16,
+  card17,
+  card18,
+  card19,
+  card20,
+  card21,
+  card22,
+  card23,
+  card24,
+  card25,
+  card26,
+  card27,
+  card28,
+  card29,
+  card30,
+  card31,
+  card32,
+  card33,
+  card34,
+  card35,
+  card36,
+  card37,
+  card38,
+  card39,
+  card40,
+  card41,
+  card42,
+  card43,
+  card44,
+  card45,
+  card46,
+  card47,
+  card48,
+  card49,
+  card50,
+  card51,
+  card52
+  ]
